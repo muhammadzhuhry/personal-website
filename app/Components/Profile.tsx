07b7b89bc553ca68f76cab0dc7d33975d275fd90
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-09-12 13:04:55 +0700

    create profile component

---
+++ app/Components/Profile.tsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div className='margin-20 text-center'>
+      <h4>Muhammad Athallah Zhuhry</h4>
+      <p>Software Engineer | Backend Developer</p>
+    </div>
+  )
+}
