32b2094d1f2d96cdf6629fdaa9cd79fbd75047c8
muhammadzhuhry <muhammadzhuhry@gmail.com>
2024-06-15 13:03:26 +0700

    add new profile image

--- app/Components/ProfileImage.tsx
+++ app/Components/ProfileImage.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import photo from '../../public/avatar.png'
+import avatar from '../../public/avatartion.png'
 
 export default function ProfileImage() {
   return (
     <Image
       className='custom-avatar'
-      src={photo}
+      src={avatar}
       alt='muhammad zhuhry profile image'
     />
   )
